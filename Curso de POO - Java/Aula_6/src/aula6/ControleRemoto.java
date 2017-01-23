261c5a9f8791d6b905e4bc43cf241109b2940a07
leandro_correa <leandrocorreati@gmail.com>
2017-01-23 18:38:34 -0200

    1.0

---
+++ Curso de POO - Java/Aula_6/src/aula6/ControleRemoto.java
@@ -0,0 +1,148 @@
+
+package aula6;
+
+public class ControleRemoto implements Controlador
+{
+    //ATRIBUTOS
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    //METÓDO CONSTRUTOR
+    public ControleRemoto() 
+    {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+    
+    //METÓDOS ACESSORES
+    private int getVolume() 
+    {
+        return volume;
+    }
+
+    private void setVolume(int V) 
+    {
+        this.volume = V;
+    }
+
+    private boolean getLigado() 
+    {
+        return ligado;
+    }
+
+    private void setLigado(boolean l) 
+    {
+        this.ligado = l;
+    }
+
+    private boolean getTocando() 
+    {
+        return tocando;
+    }
+
+    private void setTocando(boolean t) 
+    {
+        this.tocando = t;
+    }
+
+    //METÓDOS ABSTRATOS DA INTERFACE(CONTROLADOR)IMPLEMENTADOS
+    @Override
+    public boolean ligar() 
+    {
+        setLigado(true);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean desligar() 
+    {
+        setLigado(false);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean abrirMenu() 
+    {
+        System.out.println(getLigado());
+        System.out.println(getVolume());
+        for(int i = 0; getVolume() > 10;i++)
+        {
+            System.out.println("|"); 
+        }
+        System.out.println("" + getTocando());
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean fecharMenu() 
+    {
+        System.out.println("Fechando Menu");
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int maisVolume() 
+    {
+        if (getLigado()==true)
+        {
+            setVolume(getVolume() + 1);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int menosVolume() 
+    {
+        if (getLigado()==true)
+        {
+            setVolume(getVolume() - 1);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean ligaMudo() 
+    {
+        if (getLigado()== true && getVolume() > 0 )
+        {
+           setVolume(0); 
+        }
+            
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean desligarMudo() 
+    {
+        if (getLigado()== true && getVolume() == 0 )
+        {
+           setVolume(50); 
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean play() 
+    {
+        if(getLigado() == true && getTocando() == false)
+        {
+            setTocando(true);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean pause() 
+    {
+         if(getLigado() == true && getTocando() == true)
+        {
+            setTocando(false);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ Curso de POO - Java/Aula_6/src/aula6/aula6.java
@@ -0,0 +1,20 @@
+package aula6;
+
+public class aula6 
+{
+    public static void main(String[] args) 
+    {
+        /* 
+            Os 4 pilares da programação orientada a objetos são os: 
+        
+            - Encapsulamento -> Ocultar partes independentes da implementação, permitindo construir partes invisiveis ao mundo exterior.
+                                Interface é uma lista de serviços fornecidos por um componente. É o contato com o mundo exterior, que define o que pode ser 
+                                feito com um objeto dessa classe. obs:(encapsular não é obrigatorio, mas faz parte de boas praticas)
+        
+        
+            - Polimorfismo
+            - Herança
+            - Abstração
+        */
+    }
+}
